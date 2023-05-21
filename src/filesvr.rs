45b07948f19b365e1de12283140c9b65a9c4d247
Join.G <joinhack@gmail.com>
2023-05-21 09:45:47 +0800

    support async

--- src/filesvr.rs
+++ src/filesvr.rs
@@ -88,62 +88,6 @@ where
 
 }
 
-pub struct FileServiceFuture<B> {
-    request: Request<B>,
-    local_root: String,
-}
-
-impl<B> FileServiceFuture<B> {
-    fn new(local_root: String, request: Request<B>) -> Self {
-        Self {
-            request,
-            local_root 
-        }
-    }
-}
-
-impl<B> Future for FileServiceFuture<B> {
-    type Output = Result<Response<Body>>;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let Self{
-            request: ref req,
-            ref local_root
-        } = *self;
-        let mut req_resolve = RequestResolve::new(local_root, &req);
-        let resolved = match Pin::new(&mut req_resolve).poll(cx) {
-            Poll::Ready(Ok(r)) => r,
-            Poll::Ready(Err(e)) => {
-                return Poll::Ready(Err(e))
-            },
-            Poll::Pending => return Poll::Pending,
-        };
-        let resp = match resolved {
-            Resolved::IsDirectory => Response::builder()
-                    .status(StatusCode::FORBIDDEN)
-                    .body(Body::Empty),
-            Resolved::MethodNotMatched => Response::builder()
-                .status(StatusCode::BAD_REQUEST)
-                .body(Body::Empty),
-            Resolved::NotFound => Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Body::Empty),
-            Resolved::PermissionDenied => Response::builder()
-                .status(StatusCode::FORBIDDEN)
-                .body(Body::Empty),
-            Resolved::Found(f) => ResponseBuilder::new().build(f),
-        };
-        let resp = match resp {
-            Ok(resp) => resp,
-            Err(e) => {
-                let e = Error::new(ErrorKind::Other, e);
-                return Poll::Ready(Err(e));
-            },
-        };
-        Poll::Ready(Ok(resp))
-    }
-}
-
 #[derive(Clone)]
 pub struct FileServiceMaker {
     local_root: String
--- src/request_resolve.rs
+++ src/request_resolve.rs
@@ -61,39 +61,3 @@ impl<'a, B> RequestResolve<'a, B, TokioFileReaderOpener> {
         return Ok(Resolved::Found(file_with_meta));
     }
 }
-
-impl<'a, B, T: FileReaderOpener<Output = FileWithMeta>> Future for RequestResolve<'a, B, T> {
-    type Output = Result<Resolved>;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let Self {
-            ref opener,
-            ref request,
-        } = *self;
-        match *request.method() {
-            Method::GET| Method::HEAD => {},
-            _ => return Poll::Ready(Ok(Resolved::MethodNotMatched)),
-        }
-        let path = request.uri().path();
-        let mut fut = opener.open(path);
-        let p = Pin::new(&mut fut).poll(cx);
-        let file_with_meta = match  p {
-            Poll::Ready(Ok(r)) => r,
-            Poll::Ready(Err(e)) =>  {
-                let rs = match e.kind() {
-                    
-                    ErrorKind::NotFound => Ok(Resolved::NotFound),
-                    ErrorKind::PermissionDenied => Ok(Resolved::PermissionDenied),
-                    e @ _ => Err(e.into()),
-                };
-                
-                return Poll::Ready(rs);
-            },
-            Poll::Pending => return Poll::Pending,
-        };
-        if file_with_meta.is_dir {
-            return Poll::Ready(Ok(Resolved::IsDirectory));
-        }
-        return Poll::Ready(Ok(Resolved::Found(file_with_meta)));
-    }
-}
